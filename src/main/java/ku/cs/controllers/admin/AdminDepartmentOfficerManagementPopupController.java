7546a86be476427bea68f04aca06ad22f7b81043
Gamenakub <gamelovemom26@gmail.com>
2024-08-06 21:29:14 +0700

    Add admin-officer-and-advisor-management

---
+++ src/main/java/ku/cs/controllers/admin/AdminDepartmentOfficerManagementPopupController.java
@@ -0,0 +1,16 @@
+package ku.cs.controllers.admin;
+
+import javafx.fxml.FXML;
+import ku.cs.controllers.components.BasePopup;
+
+public class AdminDepartmentOfficerManagementPopupController extends BasePopup<Object> {
+    @FXML
+    public void onCancelButton(){
+        this.close();
+    }
+
+    @FXML
+    public void onConfirmButton(){
+        this.close();
+    }
+}
