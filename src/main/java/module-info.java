be95481e25ba990470271085f41096016da5a75c
Gamenakub <gamelovemom26@gmail.com>
2024-08-06 21:41:47 +0700

    Update module-info.java

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,9 +2,18 @@ module ku.cs {
     requires javafx.controls;
     requires javafx.fxml;
 
-
     opens ku.cs.cs211671project to javafx.fxml;
     exports ku.cs.cs211671project;
+
     exports ku.cs.controllers;
+    exports ku.cs.controllers.components;
+
     opens ku.cs.controllers to javafx.fxml;
+    opens ku.cs.controllers.components to javafx.fxml;
+
+    exports ku.cs.controllers.admin;
+    opens ku.cs.controllers.admin to javafx.fxml;
+
+    exports ku.cs.controllers.student;
+    opens ku.cs.controllers.student to javafx.fxml;
 }
