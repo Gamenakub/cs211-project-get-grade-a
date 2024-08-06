3cf696c7585cba26fa43a609e7693c9485861070
sunmodza <osunchizaza@gmail.com>
2024-08-06 23:18:33 +0700

    Add department officer student popup and request fxml

---
+++ src/main/java/ku/cs/controllers/officer/department/DepartmentOfficerStudentModifyPopupController.java
@@ -0,0 +1,15 @@
+package ku.cs.controllers.officer.department;
+
+import javafx.event.ActionEvent;
+import ku.cs.controllers.components.BasePopup;
+
+public class DepartmentOfficerStudentModifyPopupController extends BasePopup<Object> {
+
+    public void onCancel(ActionEvent actionEvent) {
+        close();
+    }
+
+    public void onConfirm(ActionEvent actionEvent) {
+        close();
+    }
+}
