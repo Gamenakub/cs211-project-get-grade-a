ce7163fe912b8d9917dc0a2186b7b50e2cdb5869
Jirat Kongsakul <ji.kongsakul@gmail.com>
2024-08-30 13:28:37 +0700

    Update requestforms

---
+++ src/main/java/ku/cs/models/requestforms/AbsenceRequestForm.java
@@ -0,0 +1,50 @@
+package ku.cs.models;
+
+import java.time.LocalDateTime;
+
+public class AbsenceRequestForm extends RequestForm {
+    private String course;
+    private String year;
+    private String phoneNumber;
+    private String facebookID;
+    private String lineID;
+    private String absenceType;
+    private LocalDateTime absenceDateTimeFrom;
+    private LocalDateTime absenceDateTimeUntil;
+    private SubjectList subjects;
+
+    public AbsenceRequestForm(String requestFormId, Student student, Advisor advisor, int status, String course, String year, String phoneNumber, String facebookID, String lineID, String absenceType, LocalDateTime absenceDateTimeFrom, LocalDateTime absenceDateTimeUntil) {
+        super("ใบคำร้องขอลาป่วยหรือลากิจ",requestFormId, student, advisor, status);
+        this.course = course;
+        this.year = year;
+        this.phoneNumber = phoneNumber;
+        this.facebookID = facebookID;
+        this.lineID = lineID;
+        this.absenceType = absenceType;
+        this.absenceDateTimeFrom = absenceDateTimeFrom;
+        this.absenceDateTimeUntil = absenceDateTimeUntil;
+        this.subjects = new SubjectList();
+    }
+
+    public void setCourse(String course) { this.course = course; }
+    public void setYear(String year) { this.year = year; }
+    public void setPhoneNumber(String phoneNumber) { this.phoneNumber = phoneNumber; }
+    public void setFacebookID(String facebookID) { this.facebookID = facebookID;}
+    public void setLineID(String lineID) {this.lineID = lineID; }
+    public void setAbsenceType(String absenceType) { this.absenceType = absenceType; }
+    public void setAbsenceDateTimeFrom(LocalDateTime absenceDateTimeFrom) { this.absenceDateTimeFrom = absenceDateTimeFrom; }
+    public void setAbsenceDateTimeUntil(LocalDateTime absenceDateTimeUntil) { this.absenceDateTimeUntil = absenceDateTimeUntil; }
+    public void setSubjects(SubjectList subjects) { this.subjects = subjects; }
+    public void addSubject(Subject subject) { this.subjects.addSubject(subject); }
+
+    public String getCourse() { return course; }
+    public String getYear() { return year; }
+    public String getPhoneNumber() { return phoneNumber; }
+    public String getFacebookID() { return facebookID; }
+    public String getLineID() { return lineID;}
+    public String getAbsenceType() { return absenceType; }
+    public LocalDateTime getAbsenceDateTimeFrom() { return absenceDateTimeFrom; }
+    public LocalDateTime getAbsenceDateTimeUntil() { return absenceDateTimeUntil; }
+    public SubjectList getSubjects() { return subjects; }
+}
+
---
+++ src/main/java/ku/cs/models/requestforms/CoEnrollRequestForm.java
@@ -0,0 +1,34 @@
+package ku.cs.models;
+
+public class CoEnrollRequestForm extends RequestForm {
+    private String course;
+    private String address;
+    private SubjectList subjects;
+    private Advisor teacher;
+
+    public CoEnrollRequestForm(String requestFormID, Student student, Advisor advisor, String course, int status, String address) {
+        super("ใบคำร้องขอลงทะเบียนเรียนร่วม",requestFormID, student, advisor, status);
+        this.course = course;
+        this.address = address;
+        this.subjects = new SubjectList();
+    }
+
+    public void setCourse(String course) { this.course = course; }
+    public void setAddress(String address) { this.address = address; }
+    public void setSubjects(SubjectList subjects) { this.subjects = subjects; }
+    public void setTeacher(Advisor teacher) { this.teacher = teacher; }
+    public void addSubject(Subject subject) {
+        subjects.addSubject(subject);
+    }
+
+    public String getCourse() { return course; }
+    public String getAddress() {
+        return address;
+    }
+    public SubjectList getSubjects() {
+        return subjects;
+    }
+    public Advisor getTeacher() {
+        return teacher;
+    }
+}
