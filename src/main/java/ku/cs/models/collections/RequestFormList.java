24de41340852014f21d23d7f085c051fafa66c97
Jirat Kongsakul <ji.kongsakul@gmail.com>
2024-08-30 13:34:24 +0700

    Add RequestFormList to collections

---
+++ src/main/java/ku/cs/models/collections/RequestFormList.java
@@ -0,0 +1,19 @@
+package ku.cs.models.collections;
+
+import java.util.ArrayList;
+
+public class RequestFormList {
+    private ArrayList<RequestForm> requestForms;
+    public RequestFormList() {
+        requestForms = new ArrayList<>();
+    }
+    public void addRequestForm(RequestForm requestForm) { requestForms.add(requestForm); }
+    public ArrayList<RequestForm> getRequestForms() { return requestForms; }
+    public RequestForm findRequestFormById(String requestFormId) {
+        for (RequestForm requestForm : requestForms) {
+            if(requestForm.checkRequestFormById(requestFormId))
+                return requestForm;
+        }
+        return null;
+    }
+}
