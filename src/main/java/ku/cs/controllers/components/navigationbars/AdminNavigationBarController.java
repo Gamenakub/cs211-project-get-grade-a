4b0557ea0d1fff7d4b8d02ba0c8bab743f115afc
Tee <anusonsart@gmail.com>
2024-09-26 04:08:17 +0700

    Update Navigationbar for every role

--- src/main/java/ku/cs/controllers/components/navigationbars/AdminNavigationBarController.java
+++ src/main/java/ku/cs/controllers/components/navigationbars/AdminNavigationBarController.java
@@ -1,11 +1,35 @@
-package ku.cs.controllers.components;
+package ku.cs.controllers.components.navigationbars;
 
 import javafx.fxml.FXML;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.MenuButton;
+import javafx.scene.control.MenuItem;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.shape.Circle;
+import ku.cs.controllers.components.ProfilePictureController;
 import ku.cs.services.FXRouter;
+import ku.cs.services.Session;
 
 import java.io.IOException;
 
-public class AdminNavigatorBarController {
+public class AdminNavigationBarController {
+    @FXML
+    AnchorPane navBarAnchorPane;
+    @FXML
+    Circle profilePictureCircle;
+    @FXML
+    MenuButton fontSizeMenuButton;
+    @FXML
+    MenuItem largeItem;
+    @FXML
+    MenuItem mediumItem;
+    @FXML
+    MenuItem smallItem;
+    public void initialize() {
+        ProfilePictureController.setImageToCircle(profilePictureCircle, Session.getSession().getLoggedInUser().getProfilePictureFileName());
+    }
+
     @FXML
     public void onPersonalInformationManagementButton(){
         try {
@@ -78,13 +102,46 @@ public class AdminNavigatorBarController {
     }
 
     @FXML
+    public void onThemeButtonClick(){
+        Scene scene = navBarAnchorPane.getScene();
+        Parent root = scene.getRoot();
+        Session.getSession().getTheme().changeTheme(root);
+    }
+
+    @FXML
     public void onLogoutButton() {
+        Session.getSession().clearSession();
         try {
             FXRouter.goTo("user-login");
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
     }
+
+    @FXML
+    public void onLargeItemClick(){
+        fontSizeMenuButton.setText(largeItem.getText());
+        Scene scene = navBarAnchorPane.getScene();
+        Parent root = scene.getRoot();
+        Session.getSession().getTheme().changeFontSize(root,fontSizeMenuButton.getText());
+    }
+
+    @FXML
+    public void onSmallItemClick() {
+        fontSizeMenuButton.setText(smallItem.getText());
+        Scene scene = navBarAnchorPane.getScene();
+        Parent root = scene.getRoot();
+        Session.getSession().getTheme().changeFontSize(root,fontSizeMenuButton.getText());
+    }
+
+    @FXML
+    public void onMediumItemClick() {
+        fontSizeMenuButton.setText(mediumItem.getText());
+        Scene scene = navBarAnchorPane.getScene();
+        Parent root = scene.getRoot();
+        Session.getSession().getTheme().changeFontSize(root,fontSizeMenuButton.getText());
+    }
+
 
 
 }
