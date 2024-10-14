b6863902d2b8baec93e0259b1d96dc73e13f8d91
Jirat Kongsakul <ji.kongsakul@gmail.com>
2024-10-14 22:11:20 +0700

    Update Student, RequestForms, Course Models and Collections

--- src/main/java/ku/cs/models/collections/CourseList.java
+++ src/main/java/ku/cs/models/collections/CourseList.java
@@ -6,29 +6,25 @@ import java.util.ArrayList;
 
 public class CourseList {
     private final ArrayList<Course> courses;
+
     public CourseList() {
         courses = new ArrayList<>();
     }
+
     public void addCourse(Course course) {
-        if(!courses.contains(course)&& course != null) {
+        if (!courses.contains(course) && course != null) {
             courses.add(course);
         }
     }
-    public void removeCourse(Course course) { courses.remove(course); }
-    public ArrayList<Course> getCourses() { return courses; }
-    public int getIndexOfCourse(Course course) { return courses.indexOf(course); }
-    public Course findCourseByID(String subjectId) {
-        for(Course course : courses) {
-            if(course.checkSubjectById(subjectId)){
-                return course;
-            }
-        }
-        return null;
+
+    public ArrayList<Course> getCourses() {
+        return courses;
     }
+
     public CourseList getCourseByRelatedRequestFormId(String relatedRequestFormId) {
         CourseList relatedCourses = new CourseList();
-        for(Course course : this.courses) {
-            if(course.getRelatedRequestFormId().equals(relatedRequestFormId)) {
+        for (Course course : this.courses) {
+            if (course.getRelatedRequestFormId().equals(relatedRequestFormId)) {
                 relatedCourses.addCourse(course);
             }
         }
@@ -37,5 +33,11 @@ public class CourseList {
 
     public int getCourseListSize() {
         return courses.size();
+    }
+
+    public void setAllCourseRequestFormId(String requestId) {
+        for (Course course : courses) {
+            course.setRelatedRequestFormId(requestId);
+        }
     }
 }
