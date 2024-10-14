373d889362691cc9d3d7ae7757b3b58042f84119
Tee <anusonsart@gmail.com>
2024-10-14 22:50:14 +0700

    Update Search

--- src/main/java/ku/cs/controllers/components/SearchController.java
+++ src/main/java/ku/cs/controllers/components/SearchController.java
@@ -4,36 +4,32 @@ import javafx.scene.control.TextField;
 import ku.cs.controllers.components.tables.TableComponentController;
 import ku.cs.models.collections.Searchable;
 
-import java.io.IOException;
 import java.util.ArrayList;
 
 public class SearchController<T> {
-    private TextField searchTextField;
-    private TableComponentController tableComponentController;
-    private Searchable<T> data;
+    private final TextField searchTextField;
+    private final TableComponentController<T> tableComponentController;
+    private final Searchable<T> data;
     private String filterContext;
 
 
-    public SearchController(TextField searchTextField, TableComponentController table, Searchable<T> data) {
+    public SearchController(TextField searchTextField, TableComponentController<T> table, Searchable<T> data) {
         this.searchTextField = searchTextField;
         this.tableComponentController = table;
         this.data = data;
 
     }
+
     public void searchFilter() {
         String searchTerm = searchTextField.getText();
         ArrayList<T> resultSearch;
-        resultSearch=data.search(searchTerm);
-        if (!(filterContext ==null)) {
+        resultSearch = data.search(searchTerm);
+        if (!(filterContext == null)) {
             ArrayList<T> resultFilter;
-            resultFilter=data.filter(filterContext);
+            resultFilter = data.filter(filterContext);
             resultSearch.retainAll(resultFilter);
         }
-        try {
-            tableComponentController.setDisplayModels(resultSearch);
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
+        tableComponentController.setDisplayModels(resultSearch);
     }
 
 
@@ -41,15 +37,11 @@ public class SearchController<T> {
         this.filterContext = filterContext;
     }
 
-    public void resetFilter(){
+    public void resetFilter() {
         setFilterContext(null);
         ArrayList<T> resultSearch;
-        resultSearch=data.search("");
+        resultSearch = data.search("");
         resultSearch.retainAll(data.search(searchTextField.getText()));
-        try {
-            tableComponentController.setDisplayModels(resultSearch);
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
+        tableComponentController.setDisplayModels(resultSearch);
     }
 }
