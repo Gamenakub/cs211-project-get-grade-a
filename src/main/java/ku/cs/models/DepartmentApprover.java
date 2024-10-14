b2d9fa65a134646e7b3cf6557bba9b13ab1af4fe
sunmodza <osunchizaza@gmail.com>
2024-10-14 22:03:01 +0700

    Update DepartmentApprover and FacultyApprover model

--- src/main/java/ku/cs/models/DepartmentApprover.java
+++ src/main/java/ku/cs/models/DepartmentApprover.java
@@ -4,44 +4,70 @@ public class DepartmentApprover {
     private String name;
     private String surname;
     private String role;
-    private Department department;
+    private final Department department;
+    private String nameTitle;
 
-    public DepartmentApprover(String name, String surname, String role, Department department) {
+    public DepartmentApprover(String nameTitle, String name, String surname, String role, Department department) {
         this.name = name;
         this.surname = surname;
         this.role = role;
         this.department = department;
+        this.nameTitle = nameTitle;
     }
 
-    public String getName() { return name; }
-    public String getSurname() { return surname; }
-    public String getRole() { return role; }
-    public Department getDepartment() { return department; }
-    public Faculty getFaculty() { return department.getFaculty(); }
-    public void setRole(String role) { this.role = role; }
-    public void setDepartment(Department department) { this.department = department; }
-    public boolean isName(String name) {
-        return this.name.equals(name);
+    public String getName() {
+        return name;
     }
-    public String getRepresentativeRole(){
-        return getRole()+"ภาควิชา"+getDepartment().getName();
-    }
+
     public void setName(String name) {
         this.name = name;
     }
-    public DepartmentApprover getClone() {
-        return new DepartmentApprover(name, surname, role, department);
-    }
 
-    public boolean compareByValue(DepartmentApprover departmentApprover) {
-        return name.equals(departmentApprover.getName()) && surname.equals(departmentApprover.getSurname()) && role.equals(departmentApprover.getRole()) && department.isId(departmentApprover.getDepartment().getId());
+    public String getSurname() {
+        return surname;
     }
 
     public void setSurname(String surname) {
         this.surname = surname;
     }
 
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public Department getDepartment() {
+        return department;
+    }
+
+    public String getRepresentativeRole() {
+        return getRole() + "ภาควิชา" + getDepartment().getName();
+    }
+
+    public DepartmentApprover getClone() {
+        return new DepartmentApprover(nameTitle, name, surname, role, department);
+    }
+
     public String getFullName() {
-        return name + " " + surname;
+        return nameTitle + name + " " + surname;
+    }
+
+    public String getNameTitle() {
+        return nameTitle;
+    }
+
+    public void setNameTitle(String text) {
+        nameTitle = text;
+    }
+
+    public String getFullRepresentationString() {
+        return nameTitle + name + " " + surname + " (" + role + "ภาควิชา" + department.getName() + ")";
+    }
+
+    public boolean isLeaderOrSubLeader() {
+        return role.equals("รักษาการณ์หัวหน้า") || role.equals("หัวหน้า");
     }
 }
